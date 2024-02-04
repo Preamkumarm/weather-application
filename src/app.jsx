c5d9bf55957614f4055eb69311c2845624e1c292
lovely-sabeer <lovelysabir279@gmail.com>
2024-02-04 21:15:31 +0530

    second upload

--- src/app.jsx
+++ src/app.jsx
@@ -17,8 +17,8 @@ import Clouds from "./img/cloud.png";
 function App() {
   const [Icon, setIcon] = useState(Clear);
   const [Temp, setTemp] = useState(0);
-  const [City, setCity] = useState("");
-  const [Country, setCountry] = useState("");
+  const [City, setCity] = useState("Tenkasi");
+  const [Country, setCountry] = useState("IN");
   const [Weather, setWeather] = useState("");
   const [Wind, setWind] = useState(0);
 
@@ -44,7 +44,7 @@ function App() {
     "13d": Snow,
     "13n": Snow,
     "50d": Snow,
-    "50d": Snow,
+    "50n": Snow,
   };
   function find() {
     const App_id = "6be94f68890728a5fd01a5fd8b44ee35";
@@ -52,7 +52,7 @@ function App() {
       `https://api.openweathermap.org/data/2.5/weather?q=${City}&appid=${App_id}&units=Metric`
     );
     result.then(function (value) {
-      // console.log(value)
+      console.log(value);
       setTemp(value.data.main.temp);
       setCountry(value.data.sys.country);
       setWeather(value.data.weather[0].main);
@@ -76,21 +76,28 @@ function App() {
             />
             <img src={Search} alt="icon" onClick={find} className="s-icon" />
           </div>
-          <img src={Icon} alt="helo" className="w-pic" />
           <div className="details">
+            <div className="picture">
+              <img src={Icon} alt="helo" className="w-pic" />
+              <div className="w-name">{Weather}</div>
+            </div>
+
             <div className="heading">
               <img src={temp} alt="" className="upper-pic" />
               <div className="w-deg">{Temp}°C</div>
-              <div className="w-name">{Weather}</div>
             </div>
+
             <div className="heading">
               <img src={wind} alt="" className="upper-pic" />
               <div className="w-speed">{Wind} Km/h</div>
             </div>
+
             <div className="heading">
               <img src={locate} alt="" className="upper-pic" />
-              <div className="c-name">{City}</div>
-              <div className="c-country">{Country}</div>
+              <div className="locating">
+                <div className="c-name">{City},</div>
+                <div className="c-country">{Country}</div>
+              </div>
             </div>
           </div>
           <div className="author">devolped by sabeer</div>
