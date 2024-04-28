f4f04269ce5bab5cdb22092dcc9a754fc92b1d02
preamkumarm <preamkumarmano@gmail.com>
2024-04-28 23:13:39 +0530

    first commit

--- src/app.jsx
+++ src/app.jsx
@@ -7,20 +7,22 @@ import temp from "./img/temp.png";
 import locate from "./img/location.png";
 import wind from "./img/wind.png";
 
-import Drizzle from "./img/drizzle.png";
-import Rain from "./img/rain.png";
-import Clear from "./img/clear.png";
-import Snow from "./img/snow.png";
-import Thunderstrom from "./img/thunderstrom.png";
-import Clouds from "./img/cloud.png";
+import bg from "./img/bg2.jpg"
+import Drizzle from "./img/drizzle.jpg";
+import Rain from "./img/rain.jpg";
+import Clear from "./img/clear.jpg";
+import Snow from "./img/snow.jpg";
+import Thunderstrom from "./img/thunderstrom.jpg";
+import Clouds from "./img/cloud.jpg";
 
-function App() {
-  const [Icon, setIcon] = useState(Clear);
+function App() {  
+  const [Icon, setIcon] = useState(bg);
   const [Temp, setTemp] = useState(0);
   const [City, setCity] = useState("Tenkasi");
   const [Country, setCountry] = useState("IN");
-  const [Weather, setWeather] = useState("");
+  const [Weather, setWeather] = useState("Weather...?");
   const [Wind, setWind] = useState(0);
+  const [location, setLocation] = useState("...")
 
   function HandleCity(e) {
     setCity(e.target.value);
@@ -58,49 +60,49 @@ function App() {
       setWeather(value.data.weather[0].main);
       setWind(value.data.wind.speed);
       const weatherIconCode = value.data.weather[0].icon;
-      let sample = Weather;
       setIcon(weatherIconMap[weatherIconCode]);
+      setLocation(value.data.name)
     });
   }
 
   return (
     <>
-      <div className="container">
+      <div className="body">
+        <img src={Icon} alt="bg" className="bg" />
         <div className="main">
-          <div className="search">
-            <input
-              type="text"
-              onChange={HandleCity}
-              className="input"
-              placeholder="Search City..."
-            />
-            <img src={Search} alt="icon" onClick={find} className="s-icon" />
-          </div>
-          <div className="details">
+            <div className="search">
+              <div></div>
+              <input
+                type="text"
+                onChange={HandleCity}
+                className="input"
+                placeholder="Search City..."
+              />
+              <img src={Search} alt="icon" onClick={find} className="s-icon" />
+            </div>
             <div className="picture">
-              <img src={Icon} alt="helo" className="w-pic" />
               <div className="w-name">{Weather}</div>
             </div>
-
-            <div className="heading">
-              <img src={temp} alt="" className="upper-pic" />
-              <div className="w-deg">{Temp}°C</div>
-            </div>
-
-            <div className="heading">
-              <img src={wind} alt="" className="upper-pic" />
-              <div className="w-speed">{Wind} Km/h</div>
-            </div>
-
-            <div className="heading">
-              <img src={locate} alt="" className="upper-pic" />
-              <div className="locating">
-                <div className="c-name">{City},</div>
-                <div className="c-country">{Country}</div>
+            <div className="box">
+              <div className="in-box"></div>
+              
+              <div className="heading">
+                <img src={temp} alt="" className="upper-pic" />
+                <div className="w-deg">{Temp}°C</div>
+              </div>
+              <div className="heading">
+                <img src={wind} alt="" className="upper-pic" />
+                <div className="w-speed">{Wind} Km/h</div>
+              </div>
+              <div className="heading">
+                <img src={locate} alt="" className="upper-pic" />
+                <div className="locating">
+                  <div className="c-name">{location}, </div>
+                  <div className="c-name">{Country}</div>
+                </div>
               </div>
             </div>
-          </div>
-          <div className="author">devolped by sabeer</div>
+
         </div>
       </div>
     </>
